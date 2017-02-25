99347a28aa4d65e6c13469dc49beade664343f45
AdilHussain919 <adilhuss919@gmail.com>
2017-02-25 10:31:26 -0500

    added modules for authentication and user model

--- server/app.js
+++ server/app.js
@@ -12,6 +12,13 @@ let logger = require('morgan');
 let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
 
+// modules for authentication
+let session = require('express-session');
+let passport = require('passport');
+let passportlocal = require('passport-local');
+let LocalStrategy = passportlocal.Strategy;
+let flash = require('connect-flash'); // displays errors / login messages
+
 // import "mongoose" - required for DB Access
 let mongoose = require('mongoose');
 // URI
@@ -43,6 +50,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client')));
 
+// initialize passport and flash
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 // route redirects
 app.use('/', index);
