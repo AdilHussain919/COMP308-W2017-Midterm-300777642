d26807712de057342d57bddc77e30ce971a22d40
AdilHussain919 <adilhuss919@gmail.com>
2017-02-25 10:00:57 -0500

    added delete functionality

--- server/routes/books.js
+++ server/routes/books.js
@@ -80,10 +80,6 @@ router.get('/:id', (req, res, next) => {
 
 // POST - process the information passed from the details form and update the document
 router.post('/:id', (req, res, next) => {
-
-    /*****************
-     * ADD CODE HERE *
-     *****************/
     let id = req.params.id;
 
      let updatedBook = book({
@@ -108,10 +104,18 @@ router.post('/:id', (req, res, next) => {
 
 // GET - process the delete by user id
 router.get('/delete/:id', (req, res, next) => {
+    // get a reference to the id from the url
+    let id = req.params.id;
 
-    /*****************
-     * ADD CODE HERE *
-     *****************/
+    book.remove({_id: id}, (err) => {
+      if(err) {
+        console.log(err);
+        res.end(err);
+      } else {
+        // refresh the books list
+        res.redirect('/books');
+      }
+    });
 });
 
 
